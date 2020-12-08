4179921c3b8a0a48299b99b397c2f593d32fac90
Gayan Witharana <gayan.charith@archisoftconsulting.com>
2020-12-08 19:51:45 +0800

    add tailwindcss sample code from the website

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
 export default function Home() {
   return (
-    <div className="md:flex bg-white rounded-lg p-24 justify-center">
+    <figure class="bg-gray-100 rounded-xl p-8">
       <img
-        className="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
+        class="w-32 h-32 rounded-full mx-auto"
         src="https://picsum.photos/200/300"
+        alt=""
+        width="384"
+        height="512"
       />
-      <div className="text-center md:text-left">
-        <h2 className="text-lg">Jake Prins</h2>
-        <div className="text-purple-500">JavaScript developer</div>
-        <div className="text-gray-600">Twitter: @jakeprins_nl</div>
-        <div className="text-gray-600">www.jakeprins.com</div>
+      <div class="pt-6 space-y-4">
+        <blockquote>
+          <p class="text-lg font-semibo">
+            “Tailwind CSS is the only framework that I've seen scale on large
+            teams. It’s easy to customize, adapts to any design, and the build
+            size is tiny.”
+          </p>
+        </blockquote>
+        <figcaption>
+          <div>Sarah Dayan</div>
+          <div>Staff Engineer, Algolia</div>
+        </figcaption>
       </div>
-    </div>
+    </figure>
   );
 }
